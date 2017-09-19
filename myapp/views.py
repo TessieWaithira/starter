569018da1d9717793aa532235cf1bd990955ae89
TessieWaithira <terrywaithira1@gmail.com>
2017-09-19 17:13:53 +0300

    display a list of boards

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+
 
 # Create your views here.
+
+def home(request):
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+        boards_names.append(board.description)
+
+    response_html = '<br>'.join(boards_names)
+    return HttpResponse(response_html)
